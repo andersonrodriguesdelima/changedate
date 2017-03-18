0f31b80950a5e68fcd87711d1bcbba4d1c0d3a05
Anderson Rodrigues de Lima <anderson.rodrigues.web@gmail.com>
2017-03-18 14:33:51 -0300

    ajustando split e passando self

--- app/models/change_date.rb
+++ app/models/change_date.rb
@@ -1,12 +1,12 @@
 class ChangeDate < ApplicationRecord
-  def change_date(date, op, value)
 
+  def self.change_date(date, op, value)
     # Separando string para determinar quem é dia, mes, ano, hora, minuto
-    dia_atual     = date.split(" ").first.split("/").first.to_i
-    mes_atual     = date.split(" ").first.split("/").second.to_i
-    ano_atual     = date.split(" ").first.split("/").last.to_i
-    horas_atual   = date.split(" ").last.split(":").first#.to_i
-    minutos_atual = date.split(" ").last.split(":").last#.to_i
+    dia_atual     = date.split("T").first.split("-").first.to_i
+    mes_atual     = date.split("T").first.split("-").second.to_i
+    ano_atual     = date.split("T").first.split("-").last.to_i
+    horas_atual   = date.split("T").last.split(":").first#.to_i
+    minutos_atual = date.split("T").last.split(":").last#.to_i
 
     horas      = value.to_f / 60
     minutos    = value.to_f % 60
@@ -65,7 +65,7 @@ class ChangeDate < ApplicationRecord
     puts "O valor final é #{dias_final}/#{mes_final}/#{ano_final} #{horas_final}:#{minutos_final}"
   end
 
-  def ultimo_dia_mes(mes)
+  def self.ultimo_dia_mes(mes)
     meses_30_dias = [4, 6, 9, 11]
     meses_31_dias = [1, 3, 5, 7, 8, 10, 12]
     if meses_30_dias.include?(mes)
